e20b111c968562c4a5a4309af39677fbe6e8cc1f
Germanilu <lucianogermani93@gmail.com>
2022-06-18 11:12:26 +0200

    edit index.js

--- index.js
+++ index.js
@@ -5,27 +5,14 @@ const logger = require('./config/winston');
 const db = require('./db.js');
 const router = require('./router.js');
 
-// require('dotenv').config();
-
 const app = express();
-const PORT = process.env.PORT; //conf puerto HEROKU
-
-//CORS actions
-// Configurar cabeceras y cors
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+const PORT = process.env.PORT || 3000; //Configuramos puerto heroku
 
 //Middleware
 app.use(morgan('combined', { stream: logger.stream }));
 app.use(express.json());
 
 //Rutas
-app.get('/', (req, res) => {res.send('Bienvenidos!!!!');});
 app.use(router);
 
 //Connecting to the database
@@ -33,4 +20,4 @@ db.then(()=>{
     //Starting server
         app.listen(PORT, ()=> console.log(`Server on port ${PORT}`.bgGreen.black));
     })
-    .catch((err)=> console.log(err.message));  
+    .catch((err)=> console.log(err.message));
