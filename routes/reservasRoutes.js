96a27a5e5d22f5711a30aec6aee96049e71bb68d
Germanilu <lucianogermani93@gmail.com>
2022-06-18 10:23:12 +0200

    edit url

--- routes/reservasRoutes.js
+++ routes/reservasRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 //Importo modelo de datos
-const reservaController = require('../controllers/reservaControllers');
+const reservaController = require('../controllers/reservaController');
 
 // End-points CRUD Reservas
 router.get('/', reservaController.getAll);
 router.get('/:id', reservaController.getById);
-// router.get('/dnicliente/:dni', reservaController.getByDniCliente);
+router.get('/dnicliente/:dni', reservaController.getByDniCliente);
 // router.get('/telefono/:telefono', reservaController.getByTelefono);
 // router.get('/email/:email', reservaController.getByEmail);
 router.get('/hotel/:hotelId', reservaController.getByHotelId);
